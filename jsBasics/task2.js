e6ec9db7fba5554cb32a5ac15a2be6cd6aadfd6a
Vlad <vladviking94@gmail.com>
2025-05-01 19:57:22 +0300

    Task2

---
+++ jsBasics/task2.js
@@ -0,0 +1,12 @@
+let firstname = "Anton";
+let secondname = "Vlad";
+let greetings = "Hi, " + firstname + " and " + secondname;
+console.log(greetings);
+
+
+let greeting = `Hi, ${firstname} and ${secondname}`;
+console.log(greeting);
+
+
+
+
