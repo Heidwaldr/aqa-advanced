8f05f19a9fd49a64b0316372ee77b57f3f37a633
Vlad <vladviking94@gmail.com>
2025-05-08 17:17:59 +0300

    task2.js

---
+++ Homework5/task2.js
@@ -0,0 +1,16 @@
+console.log("Declaration function")
+function verification(age){ 
+   if (age >= 18){
+    console.log("особа доросла")
+    return true
+   } else {
+    console.log("Особа не доросла")
+    return false
+   }
+}
+
+console.log("below is result for number 25")
+verification(25)
+console.log("below is result for number 15")
+verification(15);
+
