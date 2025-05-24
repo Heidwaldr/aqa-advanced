ebfe06202764f27c124cc12e22b27da519d631fe
Vlad <vladviking94@gmail.com>
2025-05-24 23:52:16 +0300

    task5.js

--- Homework8/task5.js
+++ Homework8/task5.js
@@ -1,9 +1,17 @@
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 30,
-};
+const users = [
+  { name: "Olena", email: "olena@gmail.com", age: 28 },
+  { name: "Dmytro", email: "dmytro@gmail.com", age: 35 },
+  { name: "Ihor", email: "ihor@gmail.com", age: 22 }
+];
 
-console.log(person.country); // undefined
-person.country = "USA";
-console.log(person.country); // "USA"
+for (const { name, email, age } of users) {
+  console.log(`Name: ${name}`);
+  console.log(`Email: ${email}`);
+  console.log(`Age: ${age}`);
+  console.log('------------------');
+}
+
+
+
+
+
